e18a48bad62487626b8ea5c9138ef645b09e3dfa
Rxzera-21 <rafamarinucci2007@gmail.com>
2026-05-20 22:08:28 -0300

    Adição do início do código, variaveis e inclusão dos estudios

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_HISTORICO 3
+
+void exibirMenu();
+void consultarSaldo(float saldo);
+float realizarDeposito(float saldo);
+float realizarSaque(float saldo);
+void exibirExtrato(float historicoValores[], char historicoTipos[], int totalTransacoes, float saldoAtual);
+void registrarTransacao(float historicoValores[], char historicoTipos[], int *totalTransacoes, float valor, char tipo);
+
+
+
 
